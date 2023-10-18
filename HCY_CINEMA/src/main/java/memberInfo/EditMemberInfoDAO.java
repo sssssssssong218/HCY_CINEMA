48940bdc8921ef7c0b3142bc1f32b8959f4024a4
user <user@DESKTOP-COJ9858>
2023-10-18 17:43:39 +0900

    암호화 라이브러리 추가

--- HCY_CINEMA/src/main/java/memberInfo/EditMemberInfoDAO.java
+++ HCY_CINEMA/src/main/java/memberInfo/EditMemberInfoDAO.java
@@ -1,10 +1,16 @@
 package memberInfo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import dbConnection.DBConnection;
+
 public class EditMemberInfoDAO {
 	private static EditMemberInfoDAO emiDAO;
 	
 	public EditMemberInfoDAO() {
-		
 	}//constructor
 	
 	public static EditMemberInfoDAO getInstance() {
@@ -14,4 +20,53 @@ public class EditMemberInfoDAO {
 		
 		return emiDAO;
 	}//getInstance
-}
+	
+	public MemberVO selectLogin(MemberVO mVO) throws SQLException {
+		DBConnection db = DBConnection.getInstance();
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = db.getCon();
+			String selectLogin = "";
+		}finally {
+			db.dbClose(rs, pstmt, con);
+		}//finally
+		return mVO;
+	}//selectLogin
+	
+	public String selectFindID(MemberVO mVO) {
+		String result = "";
+		
+		return result;
+	}//selectFindID
+	
+	public String selectFindPW(MemberVO mVO) {
+		String result = "";
+		
+		return result;
+	}//selectFindPW
+	
+	public boolean updatePW(MemberVO mVO) {
+		boolean flag = false;
+		
+		return flag;
+	}//updatePW
+	
+	public MemberVO selectCheckMember(MemberVO mVO) {
+		
+		return mVO;
+	}//selectCheckMember
+	
+	public boolean selectCheckID(MemberVO mVO) {
+		boolean flag = false;
+		
+		return flag;
+	}//selectCheckID
+	
+	public void insertJoinMember(MemberVO mVO) {
+		
+	}//insertJoinMember
+}//class
