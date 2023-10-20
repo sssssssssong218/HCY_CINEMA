58db29b7af9bc802506ceff0a452aa06779c1808
user <user@DESKTOP-COJ9858>
2023-10-20 17:56:36 +0900

    얍야얍

--- HCY_CINEMA/src/main/java/manageMember/LoginDAO.java
+++ HCY_CINEMA/src/main/java/manageMember/LoginDAO.java
@@ -1,5 +1,87 @@
 package manageMember;
 
-public class LoginDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-}
+import dbConnection.DBConnection;
+
+public class LoginDAO {
+private static LoginDAO liDAO;
+
+private LoginDAO() {
+}//constructor
+
+public static LoginDAO getInstance() {
+	if(liDAO == null) {
+		liDAO = new LoginDAO();
+	}//if
+	return liDAO;
+}//getInstance
+
+public MemberVO selectLogin(MemberVO mVO) throws SQLException {
+	DBConnection db = DBConnection.getInstance();
+	
+	Connection con = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+	
+	try {
+		con = db.getCon();
+		
+		String selectLogin = "select, ID, MNAME, BIRTH, TEL, EMAIL, JOINDATE FROM MEMBER WHERE id=? and PASSWORD =?";
+		
+		pstmt = con.prepareStatement(selectLogin);
+		pstmt.setString(1, mVO.getId());
+		pstmt.setString(2, mVO.getPassword());
+		
+		rs=pstmt.executeQuery();
+		
+		if(rs.next()) {
+			mVO.setMname(rs.getString("MNAME"));
+			mVO.setBirth(rs.getString("BIRTH"));
+			mVO.setTel(rs.getString("tel"));
+			mVO.setEmail(rs.getString("email"));
+			mVO.setJoindate(rs.getDate("joindate"));
+			
+			return mVO;
+		}//if
+	}finally {
+		db.dbClose(rs, pstmt, con);
+	}//finally
+	return null;
+}//selectLogin
+
+public String selectFindID(MemberVO mVO) {
+	String result = "";
+	
+	return result;
+}//selectFindID
+
+public String selectFindPW(MemberVO mVO) {
+	String result = "";
+	
+	return result;
+}//selectFindPW
+
+public boolean updatePW(MemberVO mVO) {
+	boolean flag = false;
+	
+	return flag;
+}//updatePW
+
+public MemberVO selectCheckMember(MemberVO mVO) {
+	return mVO;
+}//selectCheckID
+
+public boolean selectCheckID(String id) {
+	boolean flag = false;
+	
+	return flag;
+}//selectCheckID
+
+public void insertJoinMember(MemberVO mVO) {
+	
+}//insertJoinMember
+}//class
--- HCY_CINEMA/src/main/java/manageMember/MemberVO.java
+++ HCY_CINEMA/src/main/java/manageMember/MemberVO.java
@@ -1,5 +1,78 @@
 package manageMember;
 
-public class MemberVO {
+import java.sql.Date;
 
+public class MemberVO {
+private String id, password, mname, birth, tel, email, status;
+private Date joindate;
+
+
+public MemberVO(String id, String password, String mname, String birth, String tel, String email, String status,
+		Date joindate) {
+	this.id = id;
+	this.password = password;
+	this.mname = mname;
+	this.birth = birth;
+	this.tel = tel;
+	this.email = email;
+	this.status = status;
+	this.joindate = joindate;
 }
+public MemberVO() {
+}
+@Override
+public String toString() {
+	return "MemberVO [id=" + id + ", password=" + password + ", mname=" + mname + ", birth=" + birth + ", tel=" + tel
+			+ ", email=" + email + ", status=" + status + ", joindate=" + joindate + "]";
+}
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getMname() {
+	return mname;
+}
+public void setMname(String mname) {
+	this.mname = mname;
+}
+public String getBirth() {
+	return birth;
+}
+public void setBirth(String birth) {
+	this.birth = birth;
+}
+public String getTel() {
+	return tel;
+}
+public void setTel(String tel) {
+	this.tel = tel;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getStatus() {
+	return status;
+}
+public void setStatus(String status) {
+	this.status = status;
+}
+public Date getJoindate() {
+	return joindate;
+}
+public void setJoindate(Date joindate) {
+	this.joindate = joindate;
+}
+
+
+}//class
