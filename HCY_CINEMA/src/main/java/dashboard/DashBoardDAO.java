5950543c56f6dade355e08ac99bafe9c1223cdda
user <user@DESKTOP-COJ9858>
2023-10-20 18:09:27 +0900

    복붙

--- HCY_CINEMA/src/main/java/dashboard/DashBoardDAO.java
+++ HCY_CINEMA/src/main/java/dashboard/DashBoardDAO.java
@@ -1,5 +1,12 @@
 package dashboard;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import dbConnection.DBConnection;
+import manageMember.MemberVO;
+
 public class DashBoardDAO {
 	private static DashBoardDAO dbDAO;
 	
@@ -14,5 +21,14 @@ public class DashBoardDAO {
 		
 		return dbDAO;
 	}//getInstance
+	
+	public List<MemberVO> selectMember(){
+		List<MemberVO> list=new ArrayList<MemberVO>();
+		
+		DBConnection db=DBConnection.getInstance();
+		
+		Connection con=null;
+		return list;
+	}
 	
 }//class
